a24b54f57dd8d3f3deb6db6a5ed7b11e80a0e834
Taku <gcobs135102@gmail.com>
2017-05-06 10:07:01 +0800

    Grid login form

--- public/src/components/Login.jsx
+++ public/src/components/Login.jsx
@@ -1,60 +1,67 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Form } from 'semantic-ui-react';
+import { Button, Form, Grid } from 'semantic-ui-react';
 import InputField from './InputField';
 
 const Login = ({ store, password, pristine, _onChange, _onSubmit }) => {
   return (
-    <div id="login">
-      <Form
-        error={store.status || password.status}
-        onSubmit={
-          e => {
-            e.preventDefault();
-            _onSubmit(store.value, password.value);
+    <Grid columns='equal'>
+      <Grid.Column>
+      </Grid.Column>
+      <Grid.Column width={8}>
+        <Form
+          error={store.status || password.status}
+          onSubmit={
+            e => {
+              e.preventDefault();
+              _onSubmit(store.value, password.value);
+            }
           }
-        }
-      >
-        <InputField
-          id="login-store"
-          validationState={store.status}
-          errMsg={store.errMsg}
-          label="Store"
-          type="text"
-          placeholder="please enter your storeID"
-          onChange={e => _onChange('store', e.target.value)}
-        />
-        <InputField
-          id="login-password"
-          validationState={password.status}
-          errMsg={password.errMsg}
-          label="Password"
-          type="password"
-          placeholder="please enter your password"
-          onChange={e => _onChange('password', e.target.value)}
-        />
-        <div className="actions">
-          <button
-            onClick={
-              e => {
-                e.preventDefault();
-                _onSubmit(store.value, password.value);
+        >
+          <InputField
+            id="login-store"
+            validationState={store.status}
+            errMsg={store.errMsg}
+            label="Store"
+            type="text"
+            placeholder="please enter your storeID"
+            onChange={e => _onChange('store', e.target.value)}
+          />
+          <InputField
+            id="login-password"
+            validationState={password.status}
+            errMsg={password.errMsg}
+            label="Password"
+            type="password"
+            placeholder="please enter your password"
+            onChange={e => _onChange('password', e.target.value)}
+          />
+          <div className="actions">
+            <Button
+              content='✔  Login'
+              positive
+              onClick={
+                e => {
+                  e.preventDefault();
+                  _onSubmit(store.value, password.value);
+                }
               }
-            }
-            className="login-btn"
-          >✔  Login</button>
-          <button
-            onClick={
-              e => {
-                e.preventDefault();
-                pristine();
+            />
+            <Button
+              content='✘  Reset'
+              onClick={
+                e => {
+                  e.preventDefault();
+                  pristine();
+                }
               }
-            }
-            className="close-btn"
-          >✘  Reset</button>
-        </div>
-      </Form>
-    </div>
+            />
+          </div>
+        </Form>
+      </Grid.Column>
+      <Grid.Column>
+      </Grid.Column>
+    </Grid>
   );
 };
 
